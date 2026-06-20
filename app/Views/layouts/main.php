bb19594a399bebea2ab9ae5cdf868641f61fc541
kitikazis <luiskitikazis2@gmail.com>
2026-06-20 15:53:29 -0500

    Agregar favicon SVG de la marca ATEMUP

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -13,6 +13,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= e($titulo ?? SITE_NAME) ?></title>
+    <link rel="icon" type="image/svg+xml" href="<?= asset('favicon.svg') ?>">
+    <link rel="mask-icon" href="<?= asset('favicon.svg') ?>" color="#ef4444">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= asset('css/global.css') ?>">
     <?php if (!empty($paginaCss)): ?>
